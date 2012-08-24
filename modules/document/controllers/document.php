6b661cb8c960f8e3fdf3a09e8e46d7ca0f3b467e
bjw0223 <bjw0223@saegeul.com>
2012-08-24 20:04:25 +0900

    임시

--- modules/document/controllers/document.php
+++ modules/document/controllers/document.php
@@ -5,11 +5,15 @@ class Document extends MX_Controller {
     public function writeform(){ 
         $this->load->view('writeform') ; 
     }
-
     public function save(){ 
     } 
-
     public function remove(){ 
+    }
+    public function somepage(){
+    	$this->load->view('somepage') ;
+    }
+    public function testpage(){
+    	$this->load->view('testpage') ;
     }
 } 
 /* End of file document.php */
