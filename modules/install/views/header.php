59fc2aa10e92fb2d39e3e3d9d25f66cab4b8ab58
jaehee <jaehee@artgrafii.com>
2012-08-25 19:34:30 +0900

    install module

---
+++ modules/install/views/header.php
@@ -0,0 +1,14 @@
+<?php $this->load->helper('url') ?>
+<?php $this->load->helper('asset') ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>새글</title>
+    <?echo css_asset('admin','bootstrap/css/bootstrap.css')?>
+    <?echo common_css_asset('jquery/css/smoothness/jquery-ui-1.8.22.custom.css')?>
+    <?echo common_js_asset('jquery/js/jquery-1.7.2.min.js')?>
+    <?echo common_js_asset('jquery/js/jquery-ui-1.8.22.custom.min.js')?>
+    <?echo js_asset('admin','bootstrap/js/bootstrap.min.js') ?>
+</head>
+<body> 
