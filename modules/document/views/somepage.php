e6e2e9034037c175487bea06e4c5dea07d21eabd
bjw0223 <bjw0223@saegeul.com>
2012-08-26 19:17:47 +0900

    writeform update 08.26 19:18

---
+++ modules/document/views/somepage.php
@@ -0,0 +1,22 @@
+<?php
+$this->load->helper('url'); 
+?>
+<html>
+
+<head>
+
+<title></title>
+<meta http-equiv="content-type" content="text/html" charset="utf-8" />
+<script
+	src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
+	type="text/javascript">
+</script>
+	<script src="<?=base_url()?>modules/document/views/js/tiny_mce/tiny_mce.js"> </script>
+
+</head>
+<body>
+
+<?php echo $_POST['mydiv'];  ?>
+
+</body>
+</html>
