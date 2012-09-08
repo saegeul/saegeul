d29969a8edd8967a261316363bafb926bd54a534
hyeong <root@saegeul.com>
2012-09-08 20:50:17 +0900

    ucloud 30%

--- modules/filebox/views/upload_list.php
+++ modules/filebox/views/upload_list.php
@@ -161,6 +161,17 @@ $(document).ready(function() {
 		autoOpen : false,
 	});
 });
+function search_confirm()
+{
+	if(document.search_form.keyword.value == '')
+	{
+		alert('검색어를 입력하세요.');
+		document.search_form.keyword.focus();
+		return;
+	}
+
+	document.search_form.submit();
+}
 </script>
 </head>
 <?php 
