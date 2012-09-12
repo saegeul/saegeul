2784fe61700cb13ac405aab8b3025201aa1e71be
wangonlee <apple@saegeul.com>
2012-09-12 15:18:58 +0900

    member 초대장발송 기능 임시 커밋

--- modules/member/views/email/welcome-html.php
+++ modules/member/views/email/welcome-html.php
@@ -1,20 +1,24 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head><title>Welcome to <?php echo $site_name; ?>!</title></head>
+<head><title>Welcome to <?php //echo $site_name; ?>!</title></head>
 <body>
 <div style="max-width: 800px; margin: 0; padding: 30px 0;">
 <table width="80%" border="0" cellpadding="0" cellspacing="0">
 <tr>
 <td width="5%"></td>
 <td align="left" width="95%" style="font: 13px/18px Arial, Helvetica, sans-serif;">
-<h2 style="font: normal 20px/23px Arial, Helvetica, sans-serif; margin: 0; padding: 0 0 18px; color: black;">Welcome to <?php echo $site_name; ?>!</h2>
+<h2 style="font: normal 20px/23px Arial, Helvetica, sans-serif; margin: 0; padding: 0 0 18px; color: black;">Welcome to <?php //echo $site_name; ?>!</h2>
 Thanks for joining <?php echo $site_name; ?>. We listed your sign in details below, make sure you keep them safe.<br />
 To open your <?php echo $site_name; ?> homepage, please follow this link:<br />
 <br />
-<big style="font: 16px/18px Arial, Helvetica, sans-serif;"><b><a href="<?php echo site_url('/auth/login/'); ?>" style="color: #3366cc;">Go to <?php echo $site_name; ?> now!</a></b></big><br />
+
+
+<?php echo "asdfasdfasdf";?>
+
+<big style="font: 16px/18px Arial, Helvetica, sans-serif;"><b><a href="<?php echo site_url('/member/login/'); ?>" style="color: #3366cc;">Go to <?php echo $site_name; ?> now!</a></b></big><br />
 <br />
 Link doesn't work? Copy the following link to your browser address bar:<br />
-<nobr><a href="<?php echo site_url('/auth/login/'); ?>" style="color: #3366cc;"><?php echo site_url('/auth/login/'); ?></a></nobr><br />
+<nobr><a href="<?php echo site_url('/auth/login/'); ?>" style="color: #3366cc;"><?php echo site_url('/member/login/'); ?></a></nobr><br />
 <br />
 <br />
 <?php if (strlen($username) > 0) { ?>Your username: <?php echo $username; ?><br /><?php } ?>
