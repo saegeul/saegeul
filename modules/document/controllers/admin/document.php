faac68e3c8b0e149eff7f66a1340497d220362c3
bjw0223 <bjw0223@saegeul.com>
2012-09-27 22:32:27 +0900

    update 27d 22h 32m

--- modules/document/controllers/admin/document.php
+++ modules/document/controllers/admin/document.php
@@ -76,7 +76,7 @@ class Document extends MX_Controller {
 
     }
 
-    public function document_list(){
+    public function document_list($page=1){
         $this->load->library('admin_tmpl') ; 
         $this->load->model('Document_model','document');
 
@@ -86,8 +86,42 @@ class Document extends MX_Controller {
             'body'=>'admin/document_list',
             'footer'=>'admin/footer'
         ) ; 
+        $page_view = 10; // a page recode number
+        $base_url = base_url(); // base url
+        $act_url = $base_url . "document/admin/document/document_list"; // act url
+        $page_per_block = 5; // a page recode moveing number
 
-        $data['result'] = $this->document->getDocumentList();
+        // return data setting
+        $data = "";
+
+        if($page < 1){
+            $page = 1;
+            $data['page'] = 1;
+        }else{
+            $data['page'] = $page;
+        }
+
+        $start_idx = ($page - 1) * $page_view;
+
+        // search keyworld
+        if($this->input->get('key') && $this->input->get('keyword')){
+            $data['key'] = $this->input->get('key');
+            $data['keyword'] = $this->input->get('keyword');
+        }else {
+            $data['key'] = "";
+            $data['keyword']= "";
+        }
+
+
+        // get files in DB
+        $data['result']=$this->document->getDocumentList($start_idx, $page_view, $data);
+        // get page tocal count
+        $data['total_record'] = count($this->document->total_entry_count($data));
+        $data['total_page'] = ceil($data['total_record'] / $page_view);
+        // url
+        $data['base_url'] = $base_url;
+        $data['act_url'] = $act_url;
+
         $str= $this->admin_tmpl->parse($section,$data); 
 
 
--- modules/document/models/document_model.php
+++ modules/document/models/document_model.php
@@ -1,66 +1,65 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed') ; 
 class Document_model extends CI_Model {
 
-	function __construct()
-	{
-		// Call the Model constructor
-		parent::__construct(); 
-	}
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct(); 
+    }
 
     public function insert($args){ 
-		$this->db->insert('documents', $args);
+        $this->db->insert('documents', $args);
     }
 
     public function getDocument($docu_id) {
-		$this->db->select('*');
-		$this->db->from('documents');
-		$this->db->where('doc_id', $docu_id);
-
-		$query = $this->db->get();
-		return $query->result();
-	}
-
-    //public function getDocumentList($list_num,$offset,$data){ 
-    public function getDocumentList(){ 
         $this->db->select('*');
-		$this->db->from('SG_documents');
+        $this->db->from('documents');
+        $this->db->where('doc_id', $docu_id);
 
-//		if($data['key'] && $data['keyword'])
-//		{
-//			$this->db->like($data['key'], $data['keyword']);
-//		}
-		$this->db->order_by("doc_id", "desc");
-//		$this->db->limit($offset, $list_num);
+        $query = $this->db->get();
+        return $query->result();
+    }
 
-		$query = $this->db->get();
-		return $query->result();
+    public function getDocumentList($list_num,$offset,$data){ 
+   // public function getDocumentList(){ 
+        $this->db->select('*');
+        $this->db->from('SG_documents');
+
+        if($data['key'] && $data['keyword'])
+        {
+            $this->db->like($data['key'], $data['keyword']);
+        }
+        $this->db->limit($offset, $list_num);
+
+        $this->db->order_by("doc_id", "desc");
+        $query = $this->db->get();
+        return $query->result();
     } 
 
-/*
-	function select_entry($list_num,$offset,$data)
-	{
-		$this->db->select('*');
-		$this->db->from('filebox');
 
-		if($data['key'] && $data['keyword'])
-		{
-			$this->db->like($data['key'], $data['keyword']);
-		}
-		$this->db->order_by("file_srl", "desc");
-		$this->db->limit($offset, $list_num);
+    function total_entry_count($data)
+    {
+        $this->db->select('*');
+        $this->db->from('SG_documents');
 
-		$query = $this->db->get();
-		return $query->result();
-	}
-*/
+        if($data['key'] && $data['keyword'])
+        {
+            $this->db->like($data['key'], $data['keyword']);
+        }
+        $this->db->order_by("doc_id", "desc");
 
-	function select_image()
-	{
-		$this->db->select('*');
-		$this->db->from('SG_filebox');
+        $query = $this->db->get();
+        return $query->result();
+    }
 
-		$query = $this->db->get();
-		return $query->result();
-	}
+
+function select_image()
+{
+    $this->db->select('*');
+    $this->db->from('SG_filebox');
+
+    $query = $this->db->get();
+    return $query->result();
+}
 }
 ?>
