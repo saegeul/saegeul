0262dde2a041a6fc697432cbdbf5a174550c3a4b
hyeong <root@saegeul.com>
2012-10-12 16:16:36 +0900

    clouddrive module modify

--- modules/clouddrive/views/admin/header.php
+++ modules/clouddrive/views/admin/header.php
@@ -1,4 +1,4 @@
 <div class="jumbotron subhead">
-    <h1>Saegeul Cloud <small> 파일을 관리하세요.</small></h1>
+    <h1>Saegeul Cloud <small> 클라우드 파일을 관리하세요.</small></h1>
 </div>
 <div class="admin_content_wrapper"> 
