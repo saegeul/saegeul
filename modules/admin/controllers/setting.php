dc0a0c593e76678c4b018672380aa1f31bfcca46
hyeong <root@saegeul.com>
2012-10-12 16:11:46 +0900

    amdin module, layout module modify

--- modules/admin/controllers/setting.php
+++ modules/admin/controllers/setting.php
@@ -1,263 +1,278 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed') ; 
 
-class setting extends MX_Controller { 
-	
-	
+class setting extends MX_Controller {
+	// DB isert user information
+	protected $username;
+	protected $email;
+	protected $uid;
+
 	function __construct()
 	{
 		parent::__construct();
-	
-		$this->load->helper(array('form', 'url'));
 		
+		$this->load->database();
+		$this->load->helper('url');
+		$this->load->helper('date');
+		$this->load->library('tank_auth');
+		
+		// get session data
+		$this->username = $this->tank_auth->get_username();
+		$this->uid = $this->tank_auth->get_user_id();
+		$this->email = $this->tank_auth->get_useremail();
+		
+		// check direct acess
+		if($this->uid == "")
+			redirect('member/login', 'refresh');
+
 	}
-	
-    public function index(){
-       $this->site();
 
-    }
+	public function index(){
+		$this->site();
 
-    public function email(){ 
-        $this->load->library('sg_layout') ; 
+	}
 
-        $this->sg_layout->layout('admin/layout') ; 
-        $this->sg_layout->module('admin') ; 
+	public function email(){
+		$this->load->library('sg_layout') ;
 
-        $this->sg_layout->add('setting/header') ; 
-       	$this->sg_layout->add('setting/sidebar') ; 
-        $this->sg_layout->add('setting/email') ; 
-        $this->sg_layout->add('setting/footer') ;
-	
-        $this->sg_layout->show() ; 
-    }
-    
-    public function setupEmail(){
-    	
-    	$params = array(
-    			'email'=>'',
-    			'email_protocol'=>'',
-    			'email_path'=>'',
-    			'smtp_port'=>'',
-    			'smtp_host'=>'',
-    			'smtp_pass'=>''
-    	    	);
-    
-    	
-    	$email = $this->input->post('email') ;
-    	$email_protocol = $this->input->post('email_protocol') ;
-    	$email_path = $this->input->post('email_lib_path') ;
-    	$smtp_port = $this->input->post('smtp_port') ;
-    	$smtp_host = $this->input->post('smtp_host') ;
-    	$smtp_pass = $this->input->post('smtp_pass') ;
-    	    	
-    
-    	$params['email'] = $email ;
-    	$params['email_protocol'] = $email_protocol ;
-    	$params['email_path'] = $email_path ;
-    	$params['smtp_port'] = $smtp_port ;
-    	$params['smtp_host'] = $smtp_host ;
-    	$params['smtp_pass'] = $smtp_pass ;
-    	 
-    	 $this->load->model('admin_model') ;
-    	$this->admin_model->save_emailset($params); 
-    
-    	$this->load->helper('file') ;
-    	$f = read_file('./modules/admin/files/email.txt') ;
-    
-    	foreach($params as $key => $value){
-    		$f = str_replace('{'.$key.'}', $value ,$f);
-    	}
-    
-    	write_file(APPPATH.'config/email.php',$f) ;
-    	
-    	 $this->load->library('sg_layout') ; 
+		$this->sg_layout->layout('admin/layout') ;
+		$this->sg_layout->module('admin') ;
 
-        $this->sg_layout->layout('admin/layout') ; 
-        $this->sg_layout->module('admin') ; 
+		$this->sg_layout->add('setting/header') ;
+		$this->sg_layout->add('setting/sidebar') ;
+		$this->sg_layout->add('setting/email') ;
+		$this->sg_layout->add('setting/footer') ;
 
-        $this->sg_layout->add('setting/header') ; 
-       	$this->sg_layout->add('setting/sidebar') ; 
-        $this->sg_layout->add('setting/general_message') ; 
-        $this->sg_layout->add('setting/footer') ;
-	
-        $this->sg_layout->show() ; 
-    	
-    	
-    	
-    }
-    
-    
-    
-    
-    
+		$this->sg_layout->show() ;
+	}
 
-    public function site(){
-     $this->load->library('sg_layout') ; 
+	public function setupEmail(){
+		 
+		$params = array(
+				'email'=>'',
+				'email_protocol'=>'',
+				'email_path'=>'',
+				'smtp_port'=>'',
+				'smtp_host'=>'',
+				'smtp_pass'=>''
+		);
 
-        $this->sg_layout->layout('admin/layout') ; 
-        $this->sg_layout->module('admin') ; 
-
-        $this->sg_layout->add('setting/header') ; 
-       	$this->sg_layout->add('setting/sidebar') ; 
-        $this->sg_layout->add('setting/site') ; 
-        $this->sg_layout->add('setting/footer') ;
-	
-        $this->sg_layout->show() ; 
-    }
-    
-    public function setupSite(){
-    	 
-    	$params = array(
-    			'title'=>'',
-    			'site_url'=>'',
-    			'on_register'=>''
-    			
-    	);
-    
-    	 
-    	$site_name = $this->input->post('site_name') ;
-    	$site_url = $this->input->post('site_url') ;
-    	$join_available = $this->input->post('join_available') ;
-    
-    	$params['title'] = $site_name ;
-    	$params['site_url'] = $site_url ;
-    	$params['on_register'] = $join_available ;
-    	
-    	$this->load->model('admin_model') ;
-      $this->admin_model->save_siteset($params);
-    
-    	/* $this->load->helper('file') ;
-    	$f = read_file('./modules/admin/files/email.txt') ;
-    
-    	foreach($params as $key => $value){
-    		$f = str_replace('{'.$key.'}', $value ,$f);
-    	}
-    
-    	write_file(APPPATH.'config/email.php',$f) ;
-     */	 
-    	 $this->load->library('sg_layout') ; 
-
-        $this->sg_layout->layout('admin/layout') ; 
-        $this->sg_layout->module('admin') ; 
-
-        $this->sg_layout->add('setting/header') ; 
-       	$this->sg_layout->add('setting/sidebar') ; 
-        $this->sg_layout->add('setting/general_message') ; 
-        $this->sg_layout->add('setting/footer') ;
-	
-        $this->sg_layout->show() ; 
-    	 
-    }
-    
-
-    public function refreshTable(){
-        $table_name = $this->input->post('table_name') ;
-        $table_path = $this->input->post('table_path') ;
-
-        $this->load->database(); 
-        $this->load->dbforge() ; 
-
-        if($this->db->table_exists($table_name)){ 
-            $this->dbforge->drop_table($table_name) ; 
-        } 
-
-        $this->load->helper('file') ; 
-        $this->load->library('sg_dbutil') ; 
-
-        $f = read_file($table_path) ; 
-        $result = $this->sg_dbutil->schema_parse($f) ; 
-        $this->sg_dbutil->create_table($table_name , $result['columns']) ;
-
-    }
-
-    public function deleteTable(){ 
-        $table_name = $this->input->post('table_name') ;
-
-        $this->load->database(); 
-        $this->load->dbforge() ; 
-
-        if($this->db->table_exists($table_name)){ 
-            $this->dbforge->drop_table($table_name) ; 
-        } 
-    }
-
-    public function dbtable(){
-
-        $this->load->helper('directory') ; 
-        $this->load->helper('file') ; 
-        $map = directory_map('./modules',2); 
+		 
+		$email = $this->input->post('email') ;
+		$email_protocol = $this->input->post('email_protocol') ;
+		$email_path = $this->input->post('email_lib_path') ;
+		$smtp_port = $this->input->post('smtp_port') ;
+		$smtp_host = $this->input->post('smtp_host') ;
+		$smtp_pass = $this->input->post('smtp_pass') ;
 
 
-        $schema_list = array() ; 
-        $file_list = array() ; 
+		$params['email'] = $email ;
+		$params['email_protocol'] = $email_protocol ;
+		$params['email_path'] = $email_path ;
+		$params['smtp_port'] = $smtp_port ;
+		$params['smtp_host'] = $smtp_host ;
+		$params['smtp_pass'] = $smtp_pass ;
 
-        foreach($map as $key => $row){ //modules list 
-           $dir_max = count($row) ;  
-           $path = './modules/'.$key.'/schemas' ;
+		$this->load->model('admin_model') ;
+		$this->admin_model->save_emailset($params);
 
-           for($i=0 ; $i < $dir_max ;$i++){ 
-               if($row[$i] == 'schemas'){ 
-                   $schema_list = directory_map($path) ; 
-                   for($k = 0 ; $k < count($schema_list) ;$k++){ 
-                       if(strpos($schema_list[$k],'xml')){
-                            $result = explode('.',$schema_list[$k]); 
-                            $file_list[$result[0]] = array('path' => $path.'/'.$schema_list[$k] , 
-                                                            'table' => $result[0], 
-                                                            'is_exists' => false 
-                                                    ); 
-                       }
-                   }
-               } 
-           } 
-        }
+		$this->load->helper('file') ;
+		$f = read_file('./modules/admin/files/email.txt') ;
 
-        $this->load->library('sg_dbutil') ; 
+		foreach($params as $key => $value){
+			$f = str_replace('{'.$key.'}', $value ,$f);
+		}
 
-        $data = array() ; 
-        
-        foreach($file_list as $key => $row){ 
-            $file_list[$key]['is_exists'] =  $this->sg_dbutil->is_exists($row['table']) ;
-        } 
+		write_file(APPPATH.'config/email.php',$f) ;
+		 
+		$this->load->library('sg_layout') ;
 
-        $data['schema_list']= $file_list ;
-        //$this->_register_admin() ; 
-        $this->load->library('sg_layout') ; 
+		$this->sg_layout->layout('admin/layout') ;
+		$this->sg_layout->module('admin') ;
 
-        $this->sg_layout->layout('admin/layout') ; 
-        $this->sg_layout->module('admin') ; 
+		$this->sg_layout->add('setting/header') ;
+		$this->sg_layout->add('setting/sidebar') ;
+		$this->sg_layout->add('setting/general_message') ;
+		$this->sg_layout->add('setting/footer') ;
 
-        $this->sg_layout->add('setting/header') ; 
-       	$this->sg_layout->add('setting/sidebar') ; 
-        $this->sg_layout->add('setting/dbtable') ; 
-        $this->sg_layout->add('setting/footer') ;
-	
-        $this->sg_layout->show($data) ; 
-    }
+		$this->sg_layout->show() ;
+		 
+		 
+		 
+	}
+
+
+
+
+
+
+	public function site(){
+		$this->load->library('sg_layout') ;
+
+		$this->sg_layout->layout('admin/layout') ;
+		$this->sg_layout->module('admin') ;
+
+		$this->sg_layout->add('setting/header') ;
+		$this->sg_layout->add('setting/sidebar') ;
+		$this->sg_layout->add('setting/site') ;
+		$this->sg_layout->add('setting/footer') ;
+
+		$this->sg_layout->show() ;
+	}
+
+	public function setupSite(){
+
+		$params = array(
+				'title'=>'',
+				'site_url'=>'',
+				'on_register'=>''
+				 
+		);
+
+
+		$site_name = $this->input->post('site_name') ;
+		$site_url = $this->input->post('site_url') ;
+		$join_available = $this->input->post('join_available') ;
+
+		$params['title'] = $site_name ;
+		$params['site_url'] = $site_url ;
+		$params['on_register'] = $join_available ;
+		 
+		$this->load->model('admin_model') ;
+		$this->admin_model->save_siteset($params);
+
+		/* $this->load->helper('file') ;
+		 $f = read_file('./modules/admin/files/email.txt') ;
+
+		foreach($params as $key => $value){
+		$f = str_replace('{'.$key.'}', $value ,$f);
+		}
+
+		write_file(APPPATH.'config/email.php',$f) ;
+		*/
+		$this->load->library('sg_layout') ;
+
+		$this->sg_layout->layout('admin/layout') ;
+		$this->sg_layout->module('admin') ;
+
+		$this->sg_layout->add('setting/header') ;
+		$this->sg_layout->add('setting/sidebar') ;
+		$this->sg_layout->add('setting/general_message') ;
+		$this->sg_layout->add('setting/footer') ;
+
+		$this->sg_layout->show() ;
+
+	}
+
+
+	public function refreshTable(){
+		$table_name = $this->input->post('table_name') ;
+		$table_path = $this->input->post('table_path') ;
+
+		$this->load->database();
+		$this->load->dbforge() ;
+
+		if($this->db->table_exists($table_name)){
+			$this->dbforge->drop_table($table_name) ;
+		}
+
+		$this->load->helper('file') ;
+		$this->load->library('sg_dbutil') ;
+
+		$f = read_file($table_path) ;
+		$result = $this->sg_dbutil->schema_parse($f) ;
+		$this->sg_dbutil->create_table($table_name , $result['columns']) ;
+
+	}
+
+	public function deleteTable(){
+		$table_name = $this->input->post('table_name') ;
+
+		$this->load->database();
+		$this->load->dbforge() ;
+
+		if($this->db->table_exists($table_name)){
+			$this->dbforge->drop_table($table_name) ;
+		}
+	}
+
+	public function dbtable(){
+
+		$this->load->helper('directory') ;
+		$this->load->helper('file') ;
+		$map = directory_map('./modules',2);
+
+
+		$schema_list = array() ;
+		$file_list = array() ;
+
+		foreach($map as $key => $row){ //modules list
+			$dir_max = count($row) ;
+			$path = './modules/'.$key.'/schemas' ;
+
+			for($i=0 ; $i < $dir_max ;$i++){
+				if($row[$i] == 'schemas'){
+					$schema_list = directory_map($path) ;
+					for($k = 0 ; $k < count($schema_list) ;$k++){
+						if(strpos($schema_list[$k],'xml')){
+							$result = explode('.',$schema_list[$k]);
+							$file_list[$result[0]] = array('path' => $path.'/'.$schema_list[$k] ,
+									'table' => $result[0],
+									'is_exists' => false
+							);
+						}
+					}
+				}
+			}
+		}
+
+		$this->load->library('sg_dbutil') ;
+
+		$data = array() ;
+
+		foreach($file_list as $key => $row){
+			$file_list[$key]['is_exists'] =  $this->sg_dbutil->is_exists($row['table']) ;
+		}
+
+		$data['schema_list']= $file_list ;
+		//$this->_register_admin() ;
+		$this->load->library('sg_layout') ;
+
+		$this->sg_layout->layout('admin/layout') ;
+		$this->sg_layout->module('admin') ;
+
+		$this->sg_layout->add('setting/header') ;
+		$this->sg_layout->add('setting/sidebar') ;
+		$this->sg_layout->add('setting/dbtable') ;
+		$this->sg_layout->add('setting/footer') ;
+
+		$this->sg_layout->show($data) ;
+	}
 }
 
 // function showshow($message)
 // {
 
 // 	$this->session->set_flashdata('message', $message);
-	
 
 
-	
+
+
 // 	$this->load->library('admin_tmpl') ;
-	 
+
 // 	$section = array(
 // 			'header'=>'setting/header',
 // 			'sidebar'=>'setting/sidebar',
 // 			'body'=>'setting/general_message',
 // 			'footer'=>'setting/footer'
 // 	) ;
-	 
+
 // 	$str= $this->admin_tmpl->parse($section, array('message' => $message));
-	 
+
 // 	echo $str ;
-	 
-	 
-	
+
+
+
 // }
 
 /* End of file setting.php */
-/* Location : ./modules/admin/setting.php */ 
+/* Location : ./modules/admin/setting.php */
--- modules/admin/views/setting/email.php
+++ modules/admin/views/setting/email.php
@@ -1,54 +1,60 @@
-<form action="setupEmail" method="post" class="form-horizontal" style="margin-top:30px;">
-    <fieldset>
-        <div class="control-group">
-            <label class="control-label" for="input01">Email Addr</label>
-            <div class="controls">
-                <input type="text" class="input-xlarge" name="email" placeholder="your_email@abc.com">
-                <p class="help-block">메일 전송을위해 사용하실 이메일 주소를 적어주세요.</p>
-            </div>
-        </div> 
-        <div class="control-group">
-            <label class="control-label" for="input02">Email Password</label>
-            <div class="controls">
-                <input type="text" class="input-xlarge" name="smtp_pass" placeholder="put your email password">
-                <p class="help-block">입력하신 이메일주소의 비밀번호를 입력해주세요.</p>
-            </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label" for="input03">Mail Protocol</label>
-            <div class="controls">
-                <select name="email_protocol">
-                    <option value="mail">Mail</option>
-                    <option value="sendmail">SendMail</option>
-                    <option value="smtp">SMTP</option>
-                </select>
-                <p class="help-block">사용하실 이메일 프로토콜을 선택해주세요.</p>
-            </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label" for="input04">SMTP HOST</label>
-            <div class="controls">
-                <input type="text" class="input-xlarge" name="smtp_host">
-                <p class="help-block">SMTP HOST를 입력해주세요.</p>
-            </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label" for="input05">SMTP PORT</label>
-            <div class="controls">
-                <input type="text" class="input-xlarge" name="smtp_port">
-                <p class="help-block">SMTP PORT를 입력해주세요.</p>
-            </div>
-        </div> 
-        <div class="control-group">
-            <label class="control-label" for="input06">Email Library Path</label>
-            <div class="controls">
-                <input type="text" class="input-xlarge" name="email_lib_path">
-                <p class="help-block">이메일 라이브러리가 존재하는 경로를 입력해주세요.</p>
-            </div>
-        </div> 
-        <div class="form-actions">
-            <button type="submit" class="btn btn-primary">Save changes</button>
-            <button class="btn">Cancel</button>
-        </div>
-    </fieldset>
-</form>
+<br>
+<br>
+<div class="content">
+	<form action="setupEmail" method="post" class="form-horizontal">
+		<fieldset>
+			<div class="control-group">
+				<label class="control-label" for="input01">Email Addr</label>
+				<div class="controls">
+					<input type="text" class="input-xlarge" name="email"
+						placeholder="your_email@abc.com">
+					<p class="help-block">메일 전송을위해 사용하실 이메일 주소를 적어주세요.</p>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="input02">Email Password</label>
+				<div class="controls">
+					<input type="text" class="input-xlarge" name="smtp_pass"
+						placeholder="put your email password">
+					<p class="help-block">입력하신 이메일주소의 비밀번호를 입력해주세요.</p>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="input03">Mail Protocol</label>
+				<div class="controls">
+					<select name="email_protocol">
+						<option value="mail">Mail</option>
+						<option value="sendmail">SendMail</option>
+						<option value="smtp">SMTP</option>
+					</select>
+					<p class="help-block">사용하실 이메일 프로토콜을 선택해주세요.</p>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="input04">SMTP HOST</label>
+				<div class="controls">
+					<input type="text" class="input-xlarge" name="smtp_host">
+					<p class="help-block">SMTP HOST를 입력해주세요.</p>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="input05">SMTP PORT</label>
+				<div class="controls">
+					<input type="text" class="input-xlarge" name="smtp_port">
+					<p class="help-block">SMTP PORT를 입력해주세요.</p>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="input06">Email Library Path</label>
+				<div class="controls">
+					<input type="text" class="input-xlarge" name="email_lib_path">
+					<p class="help-block">이메일 라이브러리가 존재하는 경로를 입력해주세요.</p>
+				</div>
+			</div>
+			<div class="form-actions">
+				<button type="submit" class="btn btn-primary">Save changes</button>
+				<button class="btn">Cancel</button>
+			</div>
+		</fieldset>
+	</form>
+</div>
