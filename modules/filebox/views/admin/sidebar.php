1ce20e55b37fb67dd8bbf14d58402d891800f628
hyeong <root@saegeul.com>
2012-10-28 22:02:48 +0900

    gallery

--- modules/filebox/views/admin/sidebar.php
+++ modules/filebox/views/admin/sidebar.php
@@ -1,15 +1,23 @@
-<div class="admin_sidebar_wrapper"> <div class=" bs-docs-sidebar">
-	<ul class="nav nav-list bs-docs-sidenav affix-top">
-            <li <?php if($action=='uploadForm'):?> class="active" <?endif;?>> 
-			<a href="<?=site_url("admin/filebox/uploadForm")?>"><i class="icon-chevron-right"></i>파일 업로드</a> 
-		</li>
-            <li <?php if($action=='fileList'):?> class="active" <?endif;?>> 
-			<a href="<?=site_url("admin/filebox/fileList")?>"><i class="icon-chevron-right"></i>파일목록</a> 
-		</li>
-            <li <?php if($action=='tagCloud'):?> class="active" <?endif;?>> 
-			<a href="<?=site_url("admin/filebox/tagCloud")?>"><i class="icon-chevron-right"></i>태그클라우드</a> 
-		</li>
-	</ul>
-</div>
+<div class="admin_sidebar_wrapper">
+	<div class=" bs-docs-sidebar">
+		<ul class="nav nav-list bs-docs-sidenav affix-top">
+			<li <?php if($action=='uploadForm'):?> class="active" <?endif;?>><a
+				href="<?=site_url("admin/filebox/uploadForm")?>"><i
+					class="icon-chevron-right"></i>파일 업로드</a>
+			</li>
+			<li <?php if($action=='fileList'):?> class="active" <?endif;?>><a
+				href="<?=site_url("admin/filebox/fileList")?>"><i
+					class="icon-chevron-right"></i>파일목록</a>
+			</li>
+			<li <?php if($action=='tagCloud'):?> class="active" <?endif;?>><a
+				href="<?=site_url("admin/filebox/tagCloud")?>"><i
+					class="icon-chevron-right"></i>태그클라우드</a>
+			</li>
+			<li <?php if($action=='gallery'):?> class="active" <?endif;?>><a
+				href="<?=site_url("gallery")?>"><i
+					class="icon-chevron-right"></i>갤러리</a>
+			</li>			
+		</ul>
+	</div>
 </div>
 
