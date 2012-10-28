1235b2e99041376ab021c99f8d4972569a7bb4ad
hyeong <root@saegeul.com>
2012-10-28 17:02:55 +0900

    modify

--- modules/admin_mgr/views/setting/openapi.php
+++ modules/admin_mgr/views/setting/openapi.php
@@ -1,35 +1,75 @@
+<?php $this->load->helper('asset') ?>
+<?echo js_asset('admin_mgr','jquery.openapi.js')?>
 <div class="_content">
-<div class="well">
-    <form action="" method="post" >
-        <input type="text" name="provider"/>
-        <input type="text" name="api_key" />
-        <input type="text" name="secret_key" /> 
-    </form>
-</div>
+	<div class="well form-inline" style="height: 30px;">
+		<input type="text" id="provider" placeholder="Provider" /> <input
+			type="text" id="api_key" placeholder="API KEY" /> <input type="text"
+			id="secret_key" placeholder="SECRET KEY" />
+		<button class="btn btn-primary btnCreateOpenApi">등록</button>
+	</div>
 
-<table class="table table-stripped">
-    <thead>
-        <th>#</th>
-        <th>Provider</th>
-        <th>API KEY</th>
-        <th>SECRET KEY</th>
-        <th>OAuth Token </th>
-        <th>OAuth Secret Token </th>
-        <th>인증 </th>
-    </thead>
-    <tbody>
-        
-        <?php foreach($api_key_list as $key => $_api_info):?>
-        <tr> 
-            <td><?php echo $_api_info->openapi_id; ?> </td>
-            <td><?php echo $_api_info->provider; ?> </td>
-            <td><?php echo $_api_info->api_key; ?> </td>
-            <td><?php echo $_api_info->secret_key; ?> </td>
-            <td><?php echo $_api_info->oauth_toekn; ?> </td>
-            <td><?php echo $_api_info->oauth_secret_token; ?> </td>
-            <td><a>인증</a></td>
-        </tr> 
-        <?php endforeach ;?>
-    </tbody> 
-</table>
+	<table class="table table-stripped">
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Provider</th>
+				<th>API KEY</th>
+				<th>SECRET KEY</th>
+				<th>OAuth Token</th>
+				<th>OAuth Secret Token</th>
+				<th>인증</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($api_key_list as $key => $_api_info):?>
+			<tr id="<?php echo $_api_info->openapi_id; ?>">
+				<td><?php echo $_api_info->openapi_id; ?></td>
+				<td><?php echo $_api_info->provider; ?></td>
+				<td><?php echo $_api_info->api_key; ?></td>
+				<td><?php echo $_api_info->secret_key; ?></td>
+				<td><?php echo $_api_info->oauth_token; ?></td>
+				<td><?php echo $_api_info->oauth_secret_token; ?></td>
+				<td><a class="btn btn-link btnCheckAuth"
+					style='color: #333333;'><i class="icon-user"></i>인증</a> <a
+					class="btn btn-link btnEditOpenApi" style='color: #333333;'><i
+						class="icon-pencil"></i>Edit</a> <a
+					class="btn btn-link btnDeleteOpenApi" style='color: #333333;'><i
+						class="icon-trash"></i>Delete</a>
+				</td>
+			</tr>
+			<?php endforeach ;?>
+		</tbody>
+	</table>
+</div>
+<div class="modal hide fade" id="openApiModal" style="width: 490px;">
+	<div class="modal-header">
+		<h3>Edit OpenApi</h3>
+	</div>
+	<div class="modal-body">
+		<div class="form-horizontal" style='margin-left: -60px;'>
+			<input type="hidden" id="inputOpenapiId">
+			<div class="control-group">
+				<label class="control-label">Provider</label>
+				<div class="controls">
+					<input type="text" id="inputProvider">
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label">API KEY</label>
+				<div class="controls">
+					<input type="text" id="inputApiKey">
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label">SECRET KEY</label>
+				<div class="controls">
+					<input type="text" id="inputSecretKey">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal-footer">
+		<a class="btn" data-dismiss="modal">Close</a>
+		<a href="javascript:void(0)" class="btn btn-primary modifyData">수정</a>
+	</div>
 </div>
--- modules/document/models/document_model.php
+++ modules/document/models/document_model.php
@@ -1,169 +1,172 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed') ; 
 class Document_model extends CI_Model {
 
-    var $table = 'documents';
-    var $table2 = 'filebox';
+	var $table = 'documents';
+	var $table2 = 'filebox';
 
-    function __construct()
-    {
-        // Call the Model constructor
-        parent::__construct(); 
-        $this->load->database();
-    }
+	function __construct()
+	{
+		// Call the Model constructor
+		parent::__construct();
+		$this->load->database();
+	}
 
-    public function insert($args){ 
-        $this->db->insert('documents', $args);
-    }
+	public function insert($args){
+		$this->db->insert('documents', $args);
+	}
 
-    public function getDocument($docu_id) {
-        $this->db->select('*');
-        $this->db->from('documents');
-        $this->db->where('doc_id', $docu_id);
+	public function getDocument($docu_id) {
+		$this->db->select('*');
+		$this->db->from('documents');
+		$this->db->where('doc_id', $docu_id);
 
-        $query = $this->db->get();
+		$query = $this->db->get();
 
-        if($query->result()){
-            $t = $query->result() ; 
-            return $t[0] ; 
-        }
+		if($query->result()){
+			$t = $query->result() ;
+			return $t[0] ;
+		}
 
-        return null ;
-    }
+		return null ;
+	}
 
-    public function getDoc($doc_id) {
-        $this->db->where("doc_id",$doc_id);
-        $query = $this->db->get($this->table);
-        $result['content'] = $query->result() ; 
+	public function getDoc($doc_id) {
+		$this->db->where("doc_id",$doc_id);
+		$query = $this->db->get($this->table);
+		$result['content'] = $query->result() ;
 
-        return $result;
-
-
-    }
-
-    public function setTrash($data,$doc_id) {
-       if( $this->db->update($this->table,$data,array('doc_id' => $doc_id ))){
-           return $doc_id;
-       }
-       return null;
-    }
-
-    public function delete($doc_id){
-
-                $this->db->where('doc_id',$doc_id);
-                $this->db->delete($this->table);
-    }
+		return $result;
 
 
-    public function getDocumentList($page=1,$list_count=10,$search_param=null,$is_trash=0){
-        $this->db->where("is_trash",$is_trash);
-        $this->db->order_by("doc_id", "desc");
-        $this->db->limit($list_count , ($page-1)*$list_count );
+	}
 
-        if($search_param == null) {
-            $query = $this->db->get($this->table);
-            //$total_rows = $this->db->count_all($this->table);
-            
-            $this->db->like("is_trash",$is_trash);
-            $total_rows = $this->db->count_all_results($this->table);
-        }else{
-            $this->db->like($search_param['search_key'],$search_param['search_keyword']);
-            $query = $this->db->get($this->table);
+	public function setTrash($data,$doc_id) {
+		if( $this->db->update($this->table,$data,array('doc_id' => $doc_id ))){
+			return $doc_id;
+		}
+		return null;
+	}
 
-            $this->db->like($search_param['search_key'],$search_param['search_keyword']);
-            $total_rows = $this->db->count_all_results($this->table);
-        }
+	public function delete($doc_id){
 
-        $pagination['page'] = $page ;
-        $pagination['list_count'] = $list_count ; 
-        $pagination['total_rows'] = $total_rows ; 
-        $pagination['page_count'] = ceil($total_rows / $list_count) ; 
-
-        $result['list'] = $query->result() ; 
-        $result['pagination'] = $pagination ; 
-
-            return $result ;
-    }
-
-    function total_entry_count($data)
-    {
-        $this->db->select('*');
-        $this->db->from('SG_documents');
-
-        if($data['key'] && $data['keyword'])
-        {
-            $this->db->like($data['key'], $data['keyword']);
-        }
-        $this->db->order_by("doc_id", "desc");
-
-        $query = $this->db->get();
-        return $query->result();
-    }
-
-    function getImageList($page=1,$list_count=10,$search_param=null){
-        $this->db->where('is_image',1);
-        $this->db->order_by("file_srl", "desc");
-        $this->db->limit($list_count , ($page-1)*$list_count );
-
-        if($search_param == null) {
-            $query = $this->db->get($this->table2);
-            $this->db->where('is_image',1);
-            $total_rows = $this->db->count_all_results($this->table2);
-        }else{
-            $this->db->like($search_param['option'],$search_param['value']);
-            $query = $this->db->get($this->table2);
-
-            $this->db->like($search_param['option'],$search_param['value']);
-            $total_rows = $this->db->count_all_results($this->table2);
-        }
-
-        $pagination['page'] = $page ;
-        $pagination['list_count'] = $list_count ; 
-        $pagination['total_rows'] = $total_rows ; 
-        $pagination['page_count'] = ceil($total_rows / $list_count) ; 
-
-        $result['list'] = $query->result() ; 
-        $result['pagination'] = $pagination ; 
-
-        return $result ;
-    }
-
-    function getFileList($page=1,$list_count=10,$search_param=null){
-        $this->db->where('is_image',0);
-        $this->db->order_by("file_srl", "desc");
-        $this->db->limit($list_count , ($page-1)*$list_count );
-
-        if($search_param == null) {
-            $query = $this->db->get($this->table2);
-            $this->db->like('is_image',0);
-            $total_rows = $this->db->count_all_results($this->table2);
-        }else{
-            $this->db->like($search_param['option'],$search_param['value']);
-            $query = $this->db->get($this->table2);
-
-            $this->db->like($search_param['option'],$search_param['value']);
-            $total_rows = $this->db->count_all_results($this->table2);
-        }
-
-        $pagination['page'] = $page ;
-        $pagination['list_count'] = $list_count ; 
-        $pagination['total_rows'] = $total_rows ; 
-        $pagination['page_count'] = ceil($total_rows / $list_count) ; 
-
-        $result['list'] = $query->result() ; 
-        $result['pagination'] = $pagination ; 
-
-        return $result ;
-    }
+		$this->db->where('doc_id',$doc_id);
+		$this->db->delete($this->table);
+	}
 
 
+	public function getDocumentList($page=1,$list_count=10,$search_param=null,$is_trash=0,$order=null){
+		$this->db->where("is_trash",$is_trash);
+		if($order != null)
+			$this->db->order_by($order, "desc");
+		else
+			$this->db->order_by("doc_id", "desc");
+		$this->db->limit($list_count , ($page-1)*$list_count );
 
-    function select_image()
-    {
-        $this->db->select('*');
-        $this->db->from('SG_filebox');
+		if($search_param == null) {
+			$query = $this->db->get($this->table);
+			//$total_rows = $this->db->count_all($this->table);
 
-        $query = $this->db->get();
-        return $query->result();
-    }
+			$this->db->like("is_trash",$is_trash);
+			$total_rows = $this->db->count_all_results($this->table);
+		}else{
+			$this->db->like($search_param['search_key'],$search_param['search_keyword']);
+			$query = $this->db->get($this->table);
+
+			$this->db->like($search_param['search_key'],$search_param['search_keyword']);
+			$total_rows = $this->db->count_all_results($this->table);
+		}
+
+		$pagination['page'] = $page ;
+		$pagination['list_count'] = $list_count ;
+		$pagination['total_rows'] = $total_rows ;
+		$pagination['page_count'] = ceil($total_rows / $list_count) ;
+
+		$result['list'] = $query->result() ;
+		$result['pagination'] = $pagination ;
+
+		return $result ;
+	}
+
+	function total_entry_count($data)
+	{
+		$this->db->select('*');
+		$this->db->from('SG_documents');
+
+		if($data['key'] && $data['keyword'])
+		{
+			$this->db->like($data['key'], $data['keyword']);
+		}
+		$this->db->order_by("doc_id", "desc");
+
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	function getImageList($page=1,$list_count=10,$search_param=null){
+		$this->db->where('is_image',1);
+		$this->db->order_by("file_srl", "desc");
+		$this->db->limit($list_count , ($page-1)*$list_count );
+
+		if($search_param == null) {
+			$query = $this->db->get($this->table2);
+			$this->db->where('is_image',1);
+			$total_rows = $this->db->count_all_results($this->table2);
+		}else{
+			$this->db->like($search_param['option'],$search_param['value']);
+			$query = $this->db->get($this->table2);
+
+			$this->db->like($search_param['option'],$search_param['value']);
+			$total_rows = $this->db->count_all_results($this->table2);
+		}
+
+		$pagination['page'] = $page ;
+		$pagination['list_count'] = $list_count ;
+		$pagination['total_rows'] = $total_rows ;
+		$pagination['page_count'] = ceil($total_rows / $list_count) ;
+
+		$result['list'] = $query->result() ;
+		$result['pagination'] = $pagination ;
+
+		return $result ;
+	}
+
+	function getFileList($page=1,$list_count=10,$search_param=null){
+		$this->db->where('is_image',0);
+		$this->db->order_by("file_srl", "desc");
+		$this->db->limit($list_count , ($page-1)*$list_count );
+
+		if($search_param == null) {
+			$query = $this->db->get($this->table2);
+			$this->db->like('is_image',0);
+			$total_rows = $this->db->count_all_results($this->table2);
+		}else{
+			$this->db->like($search_param['option'],$search_param['value']);
+			$query = $this->db->get($this->table2);
+
+			$this->db->like($search_param['option'],$search_param['value']);
+			$total_rows = $this->db->count_all_results($this->table2);
+		}
+
+		$pagination['page'] = $page ;
+		$pagination['list_count'] = $list_count ;
+		$pagination['total_rows'] = $total_rows ;
+		$pagination['page_count'] = ceil($total_rows / $list_count) ;
+
+		$result['list'] = $query->result() ;
+		$result['pagination'] = $pagination ;
+
+		return $result ;
+	}
+
+
+
+	function select_image()
+	{
+		$this->db->select('*');
+		$this->db->from('SG_filebox');
+
+		$query = $this->db->get();
+		return $query->result();
+	}
 }
 ?>
