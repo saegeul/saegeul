75fcf8250f9c1990593fc983f5b3a3f538bc7358
hyeong <root@saegeul.com>
2012-10-28 18:05:27 +0900

    AA

---
+++ application/config/blog.php
@@ -0,0 +1,20 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Blog
+| -------------------------------------------------------------------------
+| This file lets you define parameters for one page per blog number and facebook Id.
+| Please see the user guide for info:
+|
+|	http://codeigniter.com/user_guide/libraries/blog.html
+|
+*/
+$config['mailtype'] = 'html';
+$config['charset'] = 'euc-kr';
+$config['newline'] = "\r\n";
+$config['listCount']="111";
+$config['facebookId']="swww";
+
+/* End of file blog.php */
+/* Location: ./application/config/blog.php */
