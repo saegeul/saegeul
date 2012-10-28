d0d259f55fc9c0d7775cc47e520d9423f8464324
bjw0223 <bjw0223@saegeul.com>
2012-10-28 13:37:34 +0900

    gallery

--- modules/gallery/models/gallery_model.php
+++ modules/gallery/models/gallery_model.php
@@ -26,20 +26,12 @@ class Gallery_model extends CI_Model {
 		return $query->result();
 	}
 
-	public function getFileList($page=1,$list_count=10,$search_param=null){
+	public function getFileList($page=1,$list_count=10){
 		$this->db->order_by("file_srl", "desc");
 		$this->db->limit($list_count , ($page-1)*$list_count );
 	
-		if($search_param == null) {
-			$query = $this->db->get($this->table);
-			$total_rows = $this->db->count_all($this->table);
-		}else{
-			$this->db->like($search_param['search_key'],$search_param['search_keyword']);
-			$query = $this->db->get($this->table);
-	
-			$this->db->like($search_param['search_key'],$search_param['search_keyword']);
-			$total_rows = $this->db->count_all_results($this->table);
-		}
+                $query = $this->db->get($this->table);
+                $total_rows = $this->db->count_all_results($this->table);
 	
 		$pagination['page'] = $page ;
 		$pagination['list_count'] = $list_count ;
@@ -51,6 +43,26 @@ class Gallery_model extends CI_Model {
 	
 		return $result ;
 	}
+        public function getImageList($page=1,$list_count=10){
+            $this->db->where('is_image',1);
+            $this->db->order_by("file_srl", "desc");
+           // $this->db->limit($list_count , ($page-1)*$list_count );
+
+            $query = $this->db->get($this->table);
+            $this->db->where('is_image',1);
+            $total_rows = $this->db->count_all_results($this->table);
+
+            $pagination['page'] = $page ;
+            $pagination['list_count'] = $list_count ; 
+            $pagination['total_rows'] = $total_rows ; 
+            $pagination['page_count'] = ceil($total_rows / $list_count) ; 
+
+            $result['list'] = $query->result() ; 
+            $result['pagination'] = $pagination ; 
+
+            return $result ;
+
+        }
 }
 
 ?>
