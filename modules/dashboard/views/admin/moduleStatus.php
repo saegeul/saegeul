44b56a0906590f8cebd2a546d3cb2f28905b11ce
hyeong <root@saegeul.com>
2012-10-28 10:58:42 +0900

    dashboard

--- modules/dashboard/views/admin/moduleStatus.php
+++ modules/dashboard/views/admin/moduleStatus.php
@@ -1,19 +1,21 @@
 <div class="_content">
 	<div class="moduleDBInfo">
 		<h4>모듈별 현황</h4>
-		<table class="table table-condensed table-hover">
+		<table class="table table-condensed table-striped">
 			<thead>
 				<tr>
 					<th>설치된 모듈</th>
-					<th>필요 DB테이블</th>
+					<th>설치된 DB테이블</th>
+					<th>설치되지 않은 DB테이블</th>
 					<th>설치여부</th>
 				</tr>
 			</thead>
 			<?php foreach($module as $key => $value) :?>
 			<tr>
 				<td><?=$value['module_name']?></td>
-				<td><?=$value['module_schema']?></td>
-				<td><?=$value['module_schema_is_exists']?>/<?=$value['module_schema_cnt']?></td>
+				<td style='color:#468847;'><?=$value['module_schema_installed']?></td>
+				<td style='color:#B94A48;'><?=$value['module_schema_not_installed']?></td>
+				<td><?=$value['module_schema_installed_cnt']?>/<?=$value['module_schema_all_cnt']?></td>
 			</tr>
 			<?php endforeach ;?>
 		</table>
