bca7c1271009df74c2d52d3a97b0f5dc11ba5c14
jaehee <jaehee@artgrafii.com>
2012-10-28 17:29:56 +0900

    filebox update

--- modules/filebox/models/filebox_model.php
+++ modules/filebox/models/filebox_model.php
@@ -94,4 +94,32 @@ class Filebox_model extends CI_Model {
 
 		return $query->result();
 	}
+
+    public function getImageList($page=1,$list_count=10,$search_param=null){
+        $this->db->where('is_image',1);
+        $this->db->order_by("file_srl", "desc");
+        $this->db->limit($list_count , ($page-1)*$list_count );
+
+        if($search_param == null) {
+            $query = $this->db->get($this->table);
+            $this->db->where('is_image',1);
+            $total_rows = $this->db->count_all_results($this->table);
+        }else{
+            $this->db->like($search_param['option'],$search_param['value']);
+            $query = $this->db->get($this->table);
+
+            $this->db->like($search_param['option'],$search_param['value']);
+            $total_rows = $this->db->count_all_results($this->table);
+        }
+
+        $pagination['page'] = $page ;
+        $pagination['list_count'] = $list_count ; 
+        $pagination['total_rows'] = $total_rows ; 
+        $pagination['page_count'] = ceil($total_rows / $list_count) ; 
+
+        $result['list'] = $query->result() ; 
+        $result['pagination'] = $pagination ; 
+
+        return $result ;
+    }
 }
