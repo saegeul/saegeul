c5b16e21898ea5799ff3f1b63250a3190822ce18
hyeong <root@saegeul.com>
2012-10-29 12:00:37 +0900

    gallery error modify

--- modules/gallery/views/admin/gallery.php
+++ modules/gallery/views/admin/gallery.php
@@ -4,13 +4,13 @@
 <?echo js_asset('gallery','jquery.gallery.js')?>
 <?echo css_asset('gallery','style.css')?>
 
-<div id="myCarousel" class="carousel slide">
-  <!-- Carousel items -->
-      <div class="carousel-inner">
-      </div>
-  <!-- Carousel nav -->
-      <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-      <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-</div>
-<div>
+<div class="_content">
+	<div id="myCarousel" class="carousel slide">
+		<!-- Carousel items -->
+		<div class="carousel-inner"></div>
+		<!-- Carousel nav -->
+		<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+		<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+	</div>
+	<div></div>
 </div>
