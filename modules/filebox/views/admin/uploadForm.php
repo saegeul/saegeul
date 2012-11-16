131952661c57f198516252a9bc7779ded0beb168
hyeong <root@saegeul.com>
2012-11-16 20:18:46 +0900

    url

--- modules/filebox/views/admin/uploadForm.php
+++ modules/filebox/views/admin/uploadForm.php
@@ -11,7 +11,7 @@
 			<p class="dropfiletext">Drop files here</p>
 		</div>
 		<br>
-		<form id="fileupload" action="/saegeul/admin/filebox/process" method="POST"
+		<form id="fileupload" action="<?=base_url()?>admin/filebox/process" method="POST"
 			enctype="multipart/form-data">
 			<div class="row fileupload-buttonbar">
 				<div class="span7">
